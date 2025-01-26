d47b9812f4deafafe3e28c7c0a691b9c4287b1ef
Emery <bemeryb@gmail.com>
2025-01-26 22:33:01 +0300

    setup api calls

--- lib/data/remote/api/api_service.dart
+++ lib/data/remote/api/api_service.dart
@@ -1,5 +1,55 @@
 
 
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:weather_app/data/remote/api/utils.dart';
+import 'package:weather_app/data/remote/api/weather_res.dart';
 
+class DioHttpService {
+
+  late Dio _dio;
+
+
+  DioHttpService() {
+    _dio = Dio(BaseOptions(
+        baseUrl: BASE_URL,
+        //contentType: 'application/json',
+        connectTimeout: const Duration(seconds: 60),
+        receiveTimeout: const Duration(seconds: 60)));
+
+    initializeInterceptor(_dio);
+  }
+
+  initializeInterceptor(Dio dio) {
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onResponse: (response, responseInterceptorHandler) {
+          debugPrint('${response.statusCode}  \n${response.data}');
+          return responseInterceptorHandler.next(response);
+        },
+
+        onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
+
+          debugPrint(
+              '${options.method}  \n${options.path} \n${options
+                  .headers} \n${options.queryParameters} \n${options.data}');
+          return handler.next(options);
+        },
+      ),
+    );
+  }
+
+
+  NetworkResponse getAllProducts({int? cityId, int? cityName}) {
+    return apiCall(() async {
+      return _dio.get("", queryParameters: {
+        if(cityId != null) "id": cityId,
+        if(cityName != null) "q": cityName,
+        "appid": API_KEY
+      });
+    });
+  }
+
+}
 
 
